a02b26653229e05320c760ffb08624d3175de944
unknown <sheeha.kim@gmail.com>
2023-11-06 18:00:37 +0900

    Init

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/c1/a0e76ed6fd76001e138cacf436ca9e55
@@ -0,0 +1,8 @@
+package com.in28minutes.learnspringframework;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWorldConfiguration {
+	
+}
---
+++ test-lostark-api/src/main/java/com/in28minutes/learnspringframework/game/PacmanGame.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.in28minutes.learnspringframework.game;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PacmanGame implements GamingConsole{
+
+	@Override
+	public void up() {
+		System.out.println("Go UP pacman");
+	}
+
+	@Override
+	public void down() {
+		System.out.println("Go DOWN pacman");
+		
+	}
+
+	@Override
+	public void left() {
+		System.out.println("Go LEFT pacman");
+		
+	}
+
+	@Override
+	public void right() {
+		System.out.println("Go RIGHT pacman");
+		
+	}
+
+}
